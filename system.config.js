cf5dd57b88263516049e5a34d3b1cb4d73f34c05
t <t@tbarron.xyz>
2018-10-23 00:17:07 -0600

    Adding system.config.js

---
+++ system.config.js
@@ -0,0 +1,9 @@
+
+System.config({
+    paths: {
+        'react': 'https://unpkg.com/react@16.5.1/umd/react.development.js',
+        'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.5.1/umd/react-dom.production.min.js',
+        'plotly.js': 'https://cdn.plot.ly/plotly-latest.min.js'
+    }
+});
+System.import('index'); F
