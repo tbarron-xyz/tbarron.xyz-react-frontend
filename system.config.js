a03a30c1545f705a580c0abaefb19c115dd153d2
Thomas Barron <t@tbarron.xyz>
2018-10-30 06:45:37 -0600

    adding react-twitch-embed-video to system.config.js

--- system.config.js
+++ system.config.js
@@ -3,7 +3,8 @@ System.config({
     paths: {
         'react': 'https://unpkg.com/react@16.5.1/umd/react.development.js',
         'react-dom': 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.5.1/umd/react-dom.production.min.js',
-        'plotly.js': 'https://cdn.plot.ly/plotly-latest.min.js'
+        'plotly.js': 'https://cdn.plot.ly/plotly-latest.min.js',
+        'react-twitch-embed-video': 'https://cdn.jsdelivr.net/npm/react-twitch-embed-video@1.0.1/dist/index.js'
     }
 });
 System.import('index');
