01081cd55bf18d7263857d3123677eb6ac0ec5bc
Thomas Barron <t@tbarron.xyz>
2018-10-30 09:00:43 -0600

    initializing state on twitchembedthatfollowsmouse

--- components/TwitchEmbedThatFollowsMouse.tsx
+++ components/TwitchEmbedThatFollowsMouse.tsx
@@ -3,6 +3,11 @@ import TwitchRipped from './TwitchRipped';
 
 
 export default class TwitchEmbedThatFollowsMouse extends React.PureComponent<{ title: string }, { left: number, top: number }> {
+    constructor(props) {
+        super(props)
+        this.state = { left: 0, top: 0 };
+    }
+
     componentDidMount() {
         document.addEventListener('mousemove', (event) => {
             this.setState({ left: event.pageX, top: event.pageY });
